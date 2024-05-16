b3cd04fa7b98bbf6558cda1e40cb12ebb2e9c8b3
L. Kyuusei <me@lkyuusei.fr>
2024-05-16 18:55:43 +0200

    init: basics with scrolling lists for chapters

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,50 @@
+<script lang="ts">
+
+</script>
+
+<header id="header">
+    <nav>
+        <li>
+            <a href="/">Overgrown</a>
+        </li>
+    </nav>
+</header>
+
+<main id="content">
+    <slot></slot>
+</main>
+
+<footer id="footer">
+    <div class="left"></div>
+    <div class="right"></div>
+</footer>
+
+<style>
+    #header, #content, #footer {
+        display: grid;
+    }
+
+    #header {
+        background-color: var(--og-color-lgreen);
+        box-shadow: 0 0 10px 1px black;
+        border-radius: 0 0 var(--normal-spacing) var(--normal-spacing);
+    }
+
+    #footer {
+        grid-template: 'left . right' 100% / 1fr 2fr 1fr;
+
+        & > .left {
+            grid-area: left;
+            box-shadow: 0 0 10px 1px black;
+            background-color: var(--og-color-lgreen);
+            border-top-right-radius: var(--normal-spacing);
+        }
+
+        & > .right {
+            grid-area: right;
+            box-shadow: 0 0 10px 1px black;
+            background-color: var(--og-color-lgreen);
+            border-top-left-radius: var(--normal-spacing);
+        }
+    }
+</style>
